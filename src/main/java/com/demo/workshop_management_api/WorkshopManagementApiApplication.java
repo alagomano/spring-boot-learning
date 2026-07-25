65cf5487470ea4826078e1c055cb3ac679e3d0d2
alagomano <nicolas.brayan.silva@gmail.com>
2026-07-25 17:14:37 -0300

    feat: cria estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/demo/workshop_management_api/WorkshopManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.workshop_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/workshop_management_api/WorkshopManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.workshop_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
