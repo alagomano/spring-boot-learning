2c3d91ba61bcd41fbf73aceb50d2fcd7a1a82b05
alagomano <nicolas.brayan.silva@gmail.com>
2026-07-29 16:09:30 -0300

    feat: camada de serviço, registro de componentes

--- src/main/java/com/demo/workshop_management_api/resources/UserResource.java
+++ src/main/java/com/demo/workshop_management_api/resources/UserResource.java
@@ -1,18 +1,31 @@
 package com.demo.workshop_management_api.resources;
 
 import com.demo.workshop_management_api.entities.User;
+import com.demo.workshop_management_api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Maria", "maria@gmail.com", "999999999", "123");
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> findById(@PathVariable Long userId){
+        User user = userService.findById(userId);
         return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/com/demo/workshop_management_api/services/UserService.java
@@ -0,0 +1,24 @@
+package com.demo.workshop_management_api.services;
+
+import com.demo.workshop_management_api.entities.User;
+import com.demo.workshop_management_api.repositories.UseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UseRepository useRepository;
+
+    public List<User> findAll(){
+        return useRepository.findAll();
+    }
+
+    public User findById(Long userId){
+        Optional<User> user = useRepository.findById(userId);
+        return user.get();
+    }
+}
