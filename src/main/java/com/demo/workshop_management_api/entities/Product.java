325cf5b8d22a74f86980c4b7dbc8a9085493da2f
alagomano <nicolas.brayan.silva@gmail.com>
2026-07-30 14:14:59 -0300

    feat: implementa associação um-para-muitos entre Product e OrderItem

--- src/main/java/com/demo/workshop_management_api/entities/Product.java
+++ src/main/java/com/demo/workshop_management_api/entities/Product.java
@@ -1,5 +1,6 @@
 package com.demo.workshop_management_api.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -24,6 +25,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name="category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -78,6 +82,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem item : items){
+            set.add(item.getOder());
+        }
+        return set;
     }
 
     @Override
