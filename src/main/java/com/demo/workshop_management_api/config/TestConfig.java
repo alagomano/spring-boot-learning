c0da8b36c62e2381643c67b58a3eb198adb9c1c5
alagomano <nicolas.brayan.silva@gmail.com>
2026-07-30 13:53:42 -0300

    feat: implementa OrderItem para associação muitos-para-muitos com atributos extras

--- src/main/java/com/demo/workshop_management_api/config/TestConfig.java
+++ src/main/java/com/demo/workshop_management_api/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.demo.workshop_management_api.config;
 
-import com.demo.workshop_management_api.entities.Category;
-import com.demo.workshop_management_api.entities.Order;
-import com.demo.workshop_management_api.entities.Product;
-import com.demo.workshop_management_api.entities.User;
+import com.demo.workshop_management_api.entities.*;
 import com.demo.workshop_management_api.enums.OrderStatus;
-import com.demo.workshop_management_api.repositories.CategoryRepository;
-import com.demo.workshop_management_api.repositories.OrderRepository;
-import com.demo.workshop_management_api.repositories.ProductRepository;
-import com.demo.workshop_management_api.repositories.UseRepository;
+import com.demo.workshop_management_api.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -31,6 +25,9 @@ public class TestConfig implements CommandLineRunner{
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
 
     @Override
@@ -68,6 +65,13 @@ public class TestConfig implements CommandLineRunner{
         p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
     }
 }
